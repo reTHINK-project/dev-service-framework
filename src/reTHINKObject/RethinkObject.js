9b48e6be29ba17a12e9f98b0779a3c45f0658838
Paul Zühlcke <paul.benjamin.zuehlcke@fokus.fraunhofer.de>
2015-11-23 16:47:46 +0100

    Fixed RethinkObject

--- src/reTHINKObject/RethinkObject.js
+++ src/reTHINKObject/RethinkObject.js
@@ -1,25 +1,21 @@
 import tv4 from "tv4";
 
-export class RethinkObject{
+export class RethinkObject {
 
-    constructor(validation, schema){
+    constructor(validation, schema) {
 
         let _this = this;
 
         this.validation = validation;
         this.schema = schema;
-
-
-
     }
 
-
-    validate(data){
-        if(this.schema)
+    validate(data) {
+        if (this.schema)
             return tv4.validate(data, this.schema);
         else
             return false;
     }
 }
 
-export default;
+export default RethinkObject;
