80e4c4f075b6078534581716428e06c2da584118
Paul Zühlcke <paul.benjamin.zuehlcke@fokus.fraunhofer.de>
2015-11-23 16:35:20 +0100

    Fixed typos, code formatting

--- src/catalogue-factory/CatalogueDataObject.js
+++ src/catalogue-factory/CatalogueDataObject.js
@@ -1,98 +1,85 @@
 /**
  * Created by amo on 14/11/2015.
  */
-class CatalogueDataObject{
-
+class CatalogueDataObject {
     /**
-     * Creates the Cataloogue Data Object
-     * @param guid - Global Unique identifier of the Catalogue Object (e.g. Hyperty descriptor, ProtocolStub descriptor, etc) enabling the same object to be stored and discovered in different Catalogues. That means, guid corresponds to <resource-type-id> per BNF of Resource Path. Couldn't we have problems with too long URL paths?
+     * Creates the Catalogue Data Object
+     * @param guid - Global Unique identifier of the Catalogue Object (e.g. Hyperty descriptor, ProtocolStub descriptor,
+     * etc) enabling the same object to be stored and discovered in different Catalogues. That means, guid corresponds to
+     * <resource-type-id> per BNF of Resource Path. Couldn't we have problems with too long URL paths?
      * @param type - indicates the type of Catalogue Data Object e.g. Hyperty, ProtocolStub, etc
-     * @param objectName - human-understable name of the catalogue object e.g. "My Awesome Hyperty"
-     * @param description - desciption of the source package
+     * @param objectName - human-understandable name of the catalogue object e.g. "My Awesome Hyperty"
+     * @param description - description of the source package
      * @param language - the programming language used in the SourcePackage.SourceCode
-     * @param sourcePackageURL - A string containing the URL from where the source code package of the corresponding catalogue object, e.g. deployable packages containing executable code for Hyperties or ProtoStubs, can be downloaded
+     * @param sourcePackageURL - A string containing the URL from where the source code package of the corresponding
+     * catalogue object, e.g. deployable packages containing executable code for Hyperties or ProtoStubs, can be downloaded
      */
-    constructor(guid, type, objectName, description, language, sourcePackageURL){
-        this.guid = guid;
-        this.type = type;
-        this.objectName = objectName;
-        this.description = description;
-        this.language = language;
-        this.sourcePackageURL = sourcePackageURL;
+    constructor(guid, type, objectName, description, language, sourcePackageURL) {
+        this._guid = guid;
+        this._type = type;
+        this._objectName = objectName;
+        this._description = description;
+        this._language = language;
+        this._sourcePackageURL = sourcePackageURL;
 
-        this.signature = null;
-        this.sourcePackage = null;
+        this._signature = null;
+        this._sourcePackage = null;
     }
 
     // Getters
-    get guid(){ return this.guid; }
+    get guid() {
+        return this._guid;
+    }
 
-    get type(){ return this.type; }
+    get type() {
+        return this._type;
+    }
 
-    get objectName(){return this.objectName; }
+    get objectName() {
+        return this._objectName;
+    }
 
-    get description(){ return this.descriptiom; }
+    get description() {
+        return this._description;
+    }
 
-    get language() {return this.language; }
+    get language() {
+        return this._language;
+    }
 
-    get signature() { return this.signature; }
+    get signature() {
+        return this._signature;
+    }
 
-    get sourcePackage(){ return this.sourcePackage; }
-
-    get sourcePackageURL(){ return this.sourcePackageURL; }
+    get sourcePackage() {
+        return this._sourcePackage;
+    }
 
 
     // Setters
-    set guid(guid){
-        if(guid)
-            this.guid = guid;
+    /**
+     * Set the signature to enables integrity and authenticity verification
+     * @param signature
+     */
+    set signature(signature) {
+        if (signature)
+            this._signature = signature;
+
     }
 
-    set type(catalogueType){
-        if(catalogueType)
-            this.type = catalogueType;
-    }
+    set sourcePackage(srcPackage) {
+        if (srcPackage)
+            this._sourcePackage = srcPackage;
 
-    set objectName(oName){
-        if(oName)
-            this.objectName = oName;
     }
-    set description(desc){
-        if(desc)
-            this.description = desc;
-    }
-    set language(lang){
-        if(lang)
-            this.language = lang;
-    }
-
-    set signature(signature){
-        if(signature)
-            this.signature = signature;
-    }
-
-    set sourcePackage(srcPackage){
-        if(srcPackage)
-            this.sourcePackage = srcPackage;
-    }
-
-    set sourcePackageURL(srcPackageUrl){
-        if(srcPackageUrl)
-            this.sourcePackageURL = srcPackageUrl;
-    }
-
-}
-export class SourcePackage{
-    constructor(encoding, sourceCodeClassname, sourceCode, signature){
-        "use strict";
-        this.encoding = encoding;
-        this.sourceCodeClassname = sourceCodeClassname;
-        this.sourceCode = sourceCode;
-        this.signature = signature;
-    }
-
 }
 
-export var CatalogueObjectType = { HYPERTY: 'hyperty', PROTOSTUB: 'protostub', HYPERTY_RUNTIME: 'hyperty_runtime', HYPERTY_INSPECTOR: 'hyperty_inspector', HYPERTY_DATA_OBJECT: 'hyperty_data_object'};
-export var DataObjectSourceLanguage = {JAVASCRIPT_ECMA6: 'javascript_ecma6', JAVASCRIPT_ECMA5: 'javascript_ecma5', JSON_SCHEMA_V4: 'json_schema_v4', PYTHON: 'python', TYPESCRIPT: 'typescript'};
+export var CatalogueObjectType = {
+    HYPERTY: 'hyperty', PROTOSTUB: 'protostub', HYPERTY_RUNTIME: 'hyperty_runtime',
+    HYPERTY_INSPECTOR: 'hyperty_inspector', HYPERTY_DATA_OBJECT: 'hyperty_data_object'
+};
+export var DataObjectSourceLanguage = {
+    JAVASCRIPT_ECMA6: 'javascript_ecma6', JAVASCRIPT_ECMA5: 'javascript_ecma5',
+    JSON_SCHEMA_V4: 'json_schema_v4', PYTHON: 'python', TYPESCRIPT: 'typescript'
+};
 export default CatalogueDataObject;
