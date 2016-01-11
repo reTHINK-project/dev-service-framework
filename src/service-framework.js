2e36a61a41afec75b4916dab207e17b85f39a390
Robert Ende <robert.ende@fokus.fraunhofer.de>
2016-01-11 16:25:54 +0100

    updated service-framework.js, also trying to export Hyperty, Protostub and SourcePackage classes

--- src/service-framework.js
+++ src/service-framework.js
@@ -5,9 +5,13 @@ import Syncher from './syncher/Syncher';
 import DataObjectReporter from './syncher/DataObjectReporter';
 import DataObjectObserver from './syncher/DataObjectObserver';
 import HypertyConnector from './hyperty/HypertyConnector';
+import HypertyDescriptor from './catalogue-factory/HypertyDescriptor';
+import ProtocolStubDescriptor from './catalogue-factory/ProtocolStubDescriptor';
+import SourcePackage from './catalogue-factory/SourcePackage';
 
 // export {AddressFactory};
 export {CatalogueFactory};
 export {MessageFactory};
 export {Syncher, DataObjectReporter, DataObjectObserver};
 export {HypertyConnector};
+export {HypertyDescriptor, ProtocolStubDescriptor, SourcePackage};
