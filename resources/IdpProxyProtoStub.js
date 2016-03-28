3f9e11253275f49e4e1019b5c37c85d0dd46c4ce
gil <gamdias@gmail.com>
2016-03-28 14:39:20 +0100

    [idpProxyProtostub] change the redirect url

--- resources/IdpProxyProtoStub.js
+++ resources/IdpProxyProtoStub.js
@@ -3,7 +3,7 @@ let openIDConfiguration;
 let googleInfo = {
   clientSecret:          'Xx4rKucb5ZYTaXlcZX9HLfZW',
   clientID:              '808329566012-tqr8qoh111942gd2kg007t0s8f277roi.apps.googleusercontent.com',
-  redirectURI:           location.origin,
+  redirectURI:           location.href,
   issuer:                'https://accounts.google.com',
   tokenEndpoint:         'https://www.googleapis.com/oauth2/v4/token?',
   jwksUri:               'https://www.googleapis.com/oauth2/v3/certs?',
