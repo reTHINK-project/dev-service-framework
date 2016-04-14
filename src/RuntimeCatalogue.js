a293e298dcbd08461bc47a4dec4e02662408c645
Vitor Silva <vitor-t-silva@telecom.pt>
2016-04-14 17:11:28 +0100

    Update the distribution files; Update the RuntimeCatalogue file with remote and local interaction

--- src/RuntimeCatalogue.js
+++ src/RuntimeCatalogue.js
@@ -1,2 +1,4 @@
-import RuntimeCatalogue from './runtime-catalogue/RuntimeCatalogue-Local';
-export default RuntimeCatalogue;
+import RuntimeCatalogueLocal from './runtime-catalogue/RuntimeCatalogue-Local';
+import RuntimeCatalogue from './runtime-catalogue/RuntimeCatalogue';
+
+export default {RuntimeCatalogue, RuntimeCatalogueLocal};
