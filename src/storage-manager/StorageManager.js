aa60a961fd539fbc8e7b2e90cb1bb46a5658b7ad
Vitor Silva <vitorsilvaemail@gmail.com>
2016-10-27 18:59:56 +0100

    Implementation for the Dexie IndexDB wrapper

--- src/storage-manager/StorageManager.js
+++ src/storage-manager/StorageManager.js
@@ -1,54 +1,70 @@
 class StorageManager {
 
-  constructor() {
-    let store = new Map();
-    this.store = store;
+  constructor(db, storageName, version = 1) {
+    if (!db) throw Error('The Storage Manager needs the database instance');
+    if (!storageName) throw Error('The Storage Manager needs the storage name');
+
+    if (db.constructor.name !== 'Dexie') throw Error('The database instance should an Dexie instance');
+
+    let stores =  {};
+    stores[storageName] = 'key,version,value';
+
+    db.version(version).stores(stores);
+    db.open().then((db) => {
+      console.info('Found database name ' + db.name + ' with version no: ' + db.verno);
+
+      // db.tables.forEach((table) => {
+      //   console.log('Found table: ' + table.name);
+      //   console.log('Table Schema: ' + JSON.stringify(table.schema, null, 4));
+      // });
+
+    })
+    .catch('NoSuchDatabaseError', (e) => {
+      // Database with that name did not exist
+      console.error('Database not found', e);
+    }).catch((e) => {
+      console.error('Oh uh: ' + e);
+    });
+
+    this.db = db;
+    this.storageName = storageName;
   }
 
   set(key, version, value) {
-    console.log('[Service Framework] - Set:', key, version, value);
-    return new Promise((resolve, reject) => {
-      try {
-        resolve(this.store.set(key, {version: version, value: value}));
-      } catch (e) {
-        resolve(e);
-      }
-    });
+    return this.db[this.storageName].put({key:key, version:version, value:value});
   }
 
   get(key) {
-    console.log('[Service Framework] - Get:', key);
-    return new Promise((resolve, reject) => {
-      console.log('GET:', this.store.get(key));
-      try {
-        resolve(this.store.get(key));
-      } catch (e) {
-        resolve(undefined);
-      }
-    });
+    return this.db[this.storageName].where('key')
+      .equals(key)
+      .first()
+      .then(object => {
+        if (object) return object.value;
+      })
+      .catch(error => {
+        console.error('error getting the key ', key, ' with error: ', error);
+        return undefined;
+      });
   }
 
   getVersion(key) {
-    console.log('[Service Framework] - Get Version:', key);
-    return new Promise((resolve, reject) => {
-      console.log('GET:', this.store.get(key));
-      try {
-        resolve(this.store.get(key).version);
-      } catch (e) {
-        resolve(undefined);
-      }
-    });
+    return this.db[this.storageName].where('key')
+      .equals(key)
+      .first()
+      .then(object => {
+        if (object) return object.version;
+      })
+      .catch(error => {
+        console.error('error getting the version for ', key, ' with error: ', error);
+        return undefined;
+      });
   }
 
   delete(key) {
-    console.log('[Service Framework] - Delete:', key);
-    return new Promise((resolve, reject) => {
-      try {
-        resolve(this.store.delete(key));
-      } catch (e) {
-        resolve(e);
-      }
-    });
+    return this.db[this.storageName]
+      .where('key')
+      .equals(key)
+      .delete();
   }
 
 }
