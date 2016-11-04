0ab6d302e31b17e991e32107b531c731dc09dd5d
David Vílchez <dvilchez@xuaps.com>
2016-11-04 16:46:17 +0100

    Add comments and test cases to StorageManager

--- src/storage-manager/StorageManager.js
+++ src/storage-manager/StorageManager.js
@@ -27,12 +27,27 @@ class StorageManager {
     return key;
   }
 
+  /**
+ * Adds a new entry to the database for a given key, together with its version.
+ * If an entry for the given key is already stored in the database, it will be updated.
+ * @param {!string} key - key that can be used with {@link storageManager.get} to retrieve the value object
+ * @param {!string} version - version descriptor for the given value
+ * @param value - value stored in the database that is mapped to given key
+ * @returns {Promise} result - Promise that will be fulfilled with the key if the entry was stored successfully,
+ * otherwise it is rejected with an error.
+ */
   set(key, version, value) {
     console.info('[StorageManager] - set ', key);
 
     return this.db[this.storageName].put({key: this._checkKey(key), version:version, value:value});
   }
 
+/**
+ * Get a entry value from the database for a given key.
+ * If no entry is found undefined is returned.
+ * @param {!string} key - key that was stored using {@link storageManager.set}
+ * @returns {Promise} result - Promise that will be fulfilled with the value.
+ */
   get(key) {
     console.info('[StorageManager] - get ', key);
     return this.db[this.storageName].where('key')
@@ -47,6 +62,12 @@ class StorageManager {
       });
   }
 
+/**
+ * Get a entry version from the database for a given key.
+ * If no entry is found undefined is returned.
+ * @param {!string} key - key that was stored using {@link storageManager.set}
+ * @returns {Promise} result - Promise that will be fulfilled with the version.
+ */
   getVersion(key) {
     console.info('[StorageManager] - getVersion for key ', key);
     return this.db[this.storageName].where('key')
@@ -61,6 +82,11 @@ class StorageManager {
       });
   }
 
+/**
+ * Delete a entry from the database for a given key.
+ * @param {!string} key - key that was stored using {@link storageManager.set}
+ * @returns {Promise} result - Promise that will be fulfilled with the number of affected rows.
+ */
   delete(key) {
     return this.db[this.storageName]
       .where('key')
