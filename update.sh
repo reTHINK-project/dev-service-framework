8f75308489f0dddcb10a14514d7f34566bd2b47f
Vitor Silva <vitor-t-silva@telecom.pt>
2018-01-03 16:48:17 +0000

    Fix the StorageManager

--- update.sh
+++ update.sh
@@ -5,4 +5,7 @@ cp -TRv ./dist ../dev-hyperty/src/connector/node_modules/service-framework/dist
 cp -TRv ./dist ../dev-protostubs/src/protostub/slack/node_modules/service-framework/dist
 
 cp -TRv ./dist ../dev-runtime-core/node_modules/service-framework/dist
+
+cp -TRv ./dist ../dev-runtime-browser/node_modules/service-framework/dist
+
 cp -TRv ./dist ../dev-hyperty-toolkit/node_modules/service-framework/dist
