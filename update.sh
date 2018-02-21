aff8c2d219c4be046b81e937de41ac4ce2dbde3e
suil <lduarte.suil@gmail.com>
2018-02-21 15:20:45 +0000

    new dist

--- update.sh
+++ update.sh
@@ -2,12 +2,9 @@
 cp -TRv ./dist ../dev-hyperty/src/group-chat-manager/node_modules/service-framework/dist
 cp -TRv ./dist ../dev-hyperty/src/connector/node_modules/service-framework/dist
 cp -TRv ./dist ../dev-hyperty/src/user-availability/node_modules/service-framework/dist
-<<<<<<< HEAD
 
 cp -TRv ./dist ../dev-hyperty/src/observer-kwh/node_modules/service-framework/dist
 
-=======
->>>>>>> develop
 
 cp -TRv ./dist ../dev-protostubs/src/protostub/slack/node_modules/service-framework/dist
 cp -TRv ./dist ../dev-protostubs/src/protostub/vertx_app_stub/node_modules/service-framework/dist
